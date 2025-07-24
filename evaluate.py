0faf91bff0c60726decbcb0d2a7b15ccf2d860ed
Divyaansh Ranjan <145826254+divyaansh15hehe@users.noreply.github.com>
2025-07-24 14:25:50 +0530

    Update evaluate.py

--- evaluate.py
+++ evaluate.py
@@ -8,19 +8,19 @@ from PIL import Image
 from skimage.metrics import peak_signal_noise_ratio, structural_similarity
 import torchvision.transforms as T
 
-# Paths to image folders
+# Directories
 CLEAN_DIR = "gan_split/clean256"
 RECON_DIR = "gan_split/gen256"
 
 # Load LPIPS loss model
-lpips_fn = lpips.LPIPS(net='alex')  # options: 'alex', 'vgg', 'squeeze'
+lpips_fn = lpips.LPIPS(net='alex') 
 lpips_fn.eval()
 
 EPOCH = "10"
 CLEAN_PREFIX = f"clean{EPOCH}"
 RECON_PREFIX = f"gen{EPOCH}"
 
-# Preprocessing (to tensor)
+# Preprocessing
 transform = T.Compose([
     T.Resize((32, 32)),
     T.ToTensor()
@@ -30,14 +30,13 @@ def load_image(path):
     img = Image.open(path).convert("RGB")
     return transform(img)
 
-# Collect matching filenames
 file_names = sorted(os.listdir(CLEAN_DIR))
 
 psnr_scores = []
 ssim_scores = []
 lpips_scores = []
 
-for idx in range(16):  # assuming 8x8 grid → 64 images
+for idx in range(16):  # Run for number of images required , I am saving 16 images so the loop is for 16 images
     clean_path = os.path.join(CLEAN_DIR, f"{CLEAN_PREFIX}_{idx:02}.png")
     recon_path = os.path.join(RECON_DIR, f"{RECON_PREFIX}_{idx:02}.png")
 
